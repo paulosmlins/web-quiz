b4023eaa40a28dbcf313f4d97a203920684213b0
Paulo Lins <paulosilas02@hotmail.com>
2021-10-25 02:34:52 -0300

    config express server

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const { resolve } = require("path");
+const app = express;
+
+app.request("/", express.static(resolve(__dirname, "./build")));
+
+app.listen(process.env.PORT || 3000, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+});
