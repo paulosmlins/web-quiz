12ef2d278999cc357f3d04b219ed1036ca00b550
Paulo Lins <paulosilas02@hotmail.com>
2021-10-25 02:58:42 -0300

    correcting server file

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require("express");
-const { resolve } = require("path");
-const app = express;
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.request("/", express.static(resolve(__dirname, "./build")));
-
-app.listen(process.env.PORT || 3000, (err) => {
+app.use(express.static("build"));
+app.listen(PORT, (err) => {
   if (err) {
     return console.log(err);
   }
