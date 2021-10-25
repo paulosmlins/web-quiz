bc47ab035f48270a540791d7630f9901f475d304
Paulo Lins <paulosilas02@hotmail.com>
2021-10-25 10:03:39 -0300

    add get in server.js

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static("build"));
+app.use(express.static("./build"));
 app.listen(PORT, (err) => {
   if (err) {
+    path.resolve(__dirname, "client", "build", "index.html");
     return console.log(err);
   }
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
